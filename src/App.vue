6f2cb4a2ca2d122081784c0958f6db472d52e022
omarhuba <omarhuba@gmail.com>
2022-02-17 23:31:53 +0100

    added huvud-page-main and fixt template, styling added also some img in assets

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,20 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <HuvudPageMain />
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import HuvudPageMain from './components/HuvudPageMain.vue'
+export default {
+name: 'App',
+components:{ HuvudPageMain }
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
