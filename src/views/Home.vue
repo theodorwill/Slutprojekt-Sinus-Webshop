2ba8a92be32c80f5350ed2db7e662c7633295fa1
omarhuba <omarhuba@gmail.com>
2022-02-18 12:08:25 +0100

    rättat conflikt från home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,16 @@
 <template>
   <div class="home">
-<<<<<<< HEAD
-=======
-
->>>>>>> main
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 
 export default {
   name: 'Home',
   components: {
   }
-=======
 // @ is an alias to /src
 
-export default {
 
->>>>>>> main
 }
 </script>
