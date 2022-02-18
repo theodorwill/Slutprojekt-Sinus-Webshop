7fb9c59b9df812eff5deb7ea096a12f34fa2e2f2
Rashadul Haque <mohammed.haque@iths.se>
2022-02-18 13:06:09 +0100

    create views files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,14 @@
 <template>
   <div class="home">
-<<<<<<< HEAD
-=======
 
->>>>>>> main
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
+
 
 export default {
-  name: 'Home',
-  components: {
   }
-=======
-// @ is an alias to /src
 
-export default {
 
->>>>>>> main
-}
 </script>
