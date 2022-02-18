2b6b0f8724d6d509b7ad39a0b144e8e0323d4efd
omarhuba <omarhuba@gmail.com>
2022-02-18 23:59:02 +0100

    added contact view and linked to nav.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,6 +13,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/products">Products</router-link> |
       <router-link to="/about">About</router-link> |
+      <router-link to="/contact">Contact</router-link> |
       <router-link to="#">Login</router-link>
     </section>
     <router-view />
@@ -20,7 +21,10 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'contact',
+  
+};
 </script>
 
 <style scoped>
