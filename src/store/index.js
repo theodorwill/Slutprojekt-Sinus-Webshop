310a2492feaeabfa9c882b65d5b2314be75ecaf0
Omarhuba <omaruba@gmail.com>
2022-02-23 09:43:14 +0100

    fixt button nav to productlist.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as API from '@/api/mock.js' 
+// import * as API from '@/api/mock.js' 
 // import * as API from '@/api'
 
 // Data from mock.js uses for testing axios implementation only. After getting data from backend this file will be removed. 
