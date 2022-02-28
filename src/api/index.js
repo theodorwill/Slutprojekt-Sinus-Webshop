eb10f8567afc8cbe66ac6095ca21ab5720ca398b
Rashadul Haque <mohammed.haque@iths.se>
2022-02-28 19:57:34 +0100

    get-set-bugs

--- src/api/index.js
+++ src/api/index.js
@@ -90,7 +90,7 @@ export async function patchProduct(id){
 
 export async function deleteProduct(id){
   return await axios.delete('/items/'+ id)
-},
+}
 
 
 // export async function deleteProduct(id){
