522b74c06d47c16be5b009cfa967484a9ad73d1b
Omarhuba <omaruba@gmail.com>
2022-02-28 22:17:56 +0100

    header styling complate fixt

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,155 +1,163 @@
 <template>
   <div id="nav">
-    <section class="top">
-      <div class="logo" />
-      <router-link to='/category' >
-      <input type="text"  class="search" v-model="category" @change="fetchCategory(category)" placeholder="Serch by category"/>
-      </router-link>
-      
+    <div class="header">
+      <section class="top">
+        <div class="logo"></div>
+      </section>
 
-     <router-link to="/cart">
-        <button>
-          <div class="card">
-          </div>
-        </button>
-      </router-link>
+      <section class="middle">
+        <router-link to="/category">
+          <input
+            type="text"
+            class="search"
+            v-model="category"
+            @change="fetchCategory(category)"
+            placeholder="Serch by category"
+          />
+        </router-link>
+      </section>
 
-      <router-link to="/login">
-        <button>
-          <div class="login">
-          </div>
-        </button>
-      </router-link>
-    </section>
+      <section class="bottom">
+          <router-link to="/cart">
+        <div class="card">
+          <!-- <span>{{inCard.length}}</span> -->
+        </div>
+          </router-link>
+          <router-link to="/login">
+        <div class="login">
+        </div>
+          </router-link>
+      </section>
+    </div>
 
-    <section>
+    <section class="navbar">
       <router-link to="/">Home</router-link> |
       <router-link to="/products">Products</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link> 
-    
-      
-     
+      <router-link to="/contact">Contact</router-link>
     </section>
   </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex'
+import { mapActions,  } from "vuex";
 export default {
- 
-  data(){
-    return{
-      category:null
-    }
+  data() {
+    return {
+      category: null,
+    };
   },
-
-  methods:{
-    ...mapActions(["fetchCategory"])
-  }
+  computed:{
+    // ...mapGetters({inCart: "cartItems"})
+  },
+  methods: {
+    ...mapActions(["fetchCategory"]),
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.top{
-  margin: 10px;
-}
-.logo {
-  width: 200px;
-  height: 50px;
-  background-image: url("~@/assets/sinus_logo.svg");
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 100%;
-}
-.search { 
-  background-image: url("~@/assets/search.svg");
-  background-position: right top;
-  background-repeat: no-repeat;
-  background-size: contain;
-  font-size: 1.5rem;
-  cursor: pointer;
-  
-  
-  
-}
-.login {
-  padding: 0;
-  width: 100%;
-  height: 100%;
-  background-repeat: no-repeat;
-  background-size: 70%;
-  background-position: center center;
-  background-image: url("~@/assets/icon_account.svg");
-}
-.card {
-  padding: 0;
-  width: 100%;
-  height: 100%;
-  background-repeat: no-repeat;
-  background-size: 70%;
-  background-position: center center;
-  background-image: url("~@/assets/shopping-cart.svg");
-}
-
 #nav {
-  > section {
-    &:nth-child(1) {
-      display: flex;
-      flex-flow: row;
-      justify-content: center;
-      padding: 0.5rem;
-      * {
-        margin: 0 0.4rem;
-      }
-      > input {
-        width: 500px;
-        border-radius: 2rem;
-        border: 1.33px solid;
-        border-color: #ff6e40;
-      }
-      button {
-        display: flex;
-        flex-flow: column;
-        justify-content: center;
-        align-items: center;
-        width: 50px;
-        height: 50px;
-        border-radius: 0.4rem;
-        border: 1px solid;
-        border-color: #222222;
-        background-color: rgba(0, 0, 0, 0);
-        cursor: pointer;
-        transition: 0.5s;
-      }
-
-      button:hover {
-        background-color: #45a049;
-      }
+  .header{
+    display: flex;
+    justify-content: space-around;
+    padding: 1rem;
     }
-    &:last-of-type {
-      color: #ff6e40;
-      display: flex;
-      flex-flow: row;
-      justify-content: space-evenly;
-      padding: 1rem;
-      font-size: 1.1rem;
-      background-color: rgb(34, 34, 34);
-
-      > a {
-        color: #ffffff;
-        text-decoration: none;
-        &.router-link-exact-active {
-          color: #ff6e40;
-          text-decoration: underline;
-          text-decoration-thickness: 0.15rem;
-        }
-      }
-
-      & a:hover {
-        color: #ff6e40;
-      }
+  .top {
+    width: 200px;
+    height: 50px;
+    background-image: url("~@/assets/sinus_logo.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 100%;
+ 
+  }
+  .middle {
+   
+    input {
+      margin: 0;
+      padding-left: 1rem;
+      width: 500px;
+      height: 50px;
+      border-radius: 2rem;
+      border: 1.33px solid;
+      border-color: #ff6e40;
+      background-image: url("~@/assets/search.svg");
+      background-position: right top;
+      background-repeat: no-repeat;
+      background-size: contain;
+      font-size: 1.5rem;
+      cursor: pointer;
+      text-transform: capitalize;
     }
   }
+  .bottom{
+    display: flex;
+    justify-content: space-evenly;
+    width: 200px;
+    }
+    .login {
+      width: 50px;
+      height: 50px;
+      border-radius: 0.4rem;
+      border: 1px solid;
+      border-color: #222222;
+      background-color: rgba(0, 0, 0, 0);
+      cursor: pointer;
+      transition: 0.5s;
+      background-repeat: no-repeat;
+      background-size: 70%;
+      background-position: center;
+      background-image: url("~@/assets/icon_account.svg");
+      &:hover {
+        background-color: #45a049;
+        }
+    }
+    .card {
+      width: 50px;
+      height: 50px;
+      border-radius: 0.4rem;
+      border: 1px solid;
+      border-color: #222222;
+      background-color: rgba(0, 0, 0, 0);
+      cursor: pointer;
+      transition: 0.5s;
+      background-repeat: no-repeat;
+      background-size: 70%;
+      background-position: center;
+      background-image: url("~@/assets/shopping-cart.svg");
+      &:hover {
+          background-color: #45a049;
+        }
+    }
+      
+  .navbar{
+     color: #ff6e40;
+    display: flex;
+    flex-flow: row;
+    justify-content: space-evenly;
+    padding: 1rem;
+    font-size: 1.1rem;
+    background-color: rgb(34, 34, 34);
+    > a {
+      color: #ffffff;
+      text-decoration: none;
+      font-size: 1.3rem;
+      font-weight: bold;
+      &.router-link-exact-active {
+        color: #ff6e40;
+        text-decoration: underline;
+        text-decoration-thickness: 0.15rem;
+      }
+    }
+    & a:hover {
+      transition: 0.5s;
+      color: #ff6e40;
+    }
+  }
+
 }
+
+
+
 </style>
