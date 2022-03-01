aca09434cda6dca5e73a30af027c562e38d15277
Omarhuba <omaruba@gmail.com>
2022-03-01 17:01:09 +0100

    upcoming products changes to in products view

--- src/views/SingleProduct.vue
+++ src/views/SingleProduct.vue
@@ -14,6 +14,34 @@
     <SingleProductCard :product="product" />
 
     <SingleProductReview />
+  <h1 class="upcoming">Upcoming Products </h1>
+    <section class="bottom">
+      <article class="product-1">
+        <img src="../assets/new-skateboard-3.png" alt="">
+        <h2>skateboard</h2>
+        <p><span>PRICE:</span><span>SEK 599</span></p>
+        <p class="discteption">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, at.</p>
+      </article>
+      <article class="product-2">
+        <img src="../assets/new-skateboard-3.png" alt="">
+        <h2>skateboard</h2>
+        <p><span>PRICE:</span><span>SEK 599</span></p>
+        <p class="discteption">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, at.</p>
+      </article>
+      <article class="product-3">
+        <img src="../assets/new-skateboard-3.png" alt="">
+        <h2>skateboard</h2>
+        <p><span>PRICE:</span><span>SEK 599</span></p>
+        <p class="discteption">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, at.</p>
+      </article>
+      <article class="product-4">
+        <img src="../assets/new-skateboard-3.png" alt="">
+        <h2>skateboard</h2>
+        <p><span>PRICE:</span><span>SEK 599</span></p>
+        <p class="discteption">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, at.</p>
+      </article>
+     
+    </section>
   </div>
 </template>
 
@@ -52,5 +80,96 @@ export default {
     text-decoration: none;
   }
 }
+}
+.upcoming{
+  margin: 5rem 0 4rem 4rem ;
+  width: 1250px;
+  text-align: start;
+}
+.bottom {
+  width: 1250px;
+  height: 500px;
+  margin: 0 auto;
+  margin-bottom: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .product-1{
+    // background-color: #2091f9;
+    border: 1px solid black;
+    border-radius: 10px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    width: 300px;
+    margin: 2rem;
+    img{
+      width: 280px;
+      height: 350px;
+    }
+    h2{
+      text-align: center;
+    }
+    p{
+      span{
+        padding: 0.5rem;
+     } 
+    }
+  }
+  .product-2{
+     border: 1px solid black;
+    border-radius: 10px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    width: 300px;
+    margin: 2rem;
+    img{
+      width: 280px;
+      height: 350px;
+    }
+     h2{
+      text-align: center;
+    }
+    p{
+      span{
+        padding: 0.5rem;
+     } 
+    }
+  }
+  .product-3{
+     border: 1px solid black;
+    border-radius: 10px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    width: 300px;
+    margin: 2rem;
+    img{
+      width: 280px;
+      height: 350px;
+    }
+     h2{
+      text-align: center;
+    }
+    p{
+      span{
+        padding: 0.5rem;
+     } 
+    }
+  }
+  .product-4{
+    border: 1px solid black;
+    border-radius: 10px;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    width: 300px;
+    margin: 2rem;
+    img{
+      width: 280px;
+      height: 350px;
+    }
+     h2{
+      text-align: center;
+    }
+    p{
+     span{
+       padding: 0.5rem;
+     } 
+    }
+  }
 }
 </style>
